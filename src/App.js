2b81ad031af8fdfbdcedb02b32e9bb34351fbcab
Raul Maldonado <raulmaldonado.w@gmail.com>
2022-12-20 11:27:57 -0800

    implemented google auth

--- src/App.js
+++ src/App.js
@@ -1,27 +1,9 @@
 import logo from "./logo.svg";
 import "./App.css";
+import "./Firebase.js";
+import GoogleButton from 'react-google-button';
+import { signInWithGoogle } from "./Firebase";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyBvSpW9IzSWYA4nDfJvHxXRmvBtw_so82E",
-  authDomain: "mangtracker.firebaseapp.com",
-  projectId: "mangtracker",
-  storageBucket: "mangtracker.appspot.com",
-  messagingSenderId: "734044243373",
-  appId: "1:734044243373:web:a4cd98d332a3c648508d24",
-  measurementId: "G-FTN3M0NESV",
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 function App() {
   return (
@@ -39,6 +21,9 @@ function App() {
         >
           Learn React
         </a>
+        <GoogleButton type="dark" onClick={signInWithGoogle}>
+        
+        </GoogleButton>
       </header>
     </div>
   );
