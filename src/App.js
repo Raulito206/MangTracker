93e8857cfd3fd3630fa2f07e9aba860dc1c9c87c
Scott <scottdangrem@gmail.com>
2022-12-20 23:19:35 -0800

    Switched to popup login

--- src/App.js
+++ src/App.js
@@ -1,44 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
 import "./Firebase.js";
-import { useState, useEffect } from 'react'
-import { Login } from './components/Login'
-import { auth, provider } from './Firebase'
+import { useState, useEffect } from 'react';
+import { Login } from './components/Login';
+import { auth, provider } from './Firebase';
 import { getAuth, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
 
 function App() {
-  const [user, setUser] = useState({})
-
-  // useEffect(() => {
-  //   async function redirectResult() {
-  //     const result = await getRedirectResult(auth);
-  //     //This gives you a Google Access Token. You can use it to access Google APIs
-  //     try {
-  //       console.log("RESULT: " + result)
-  //       if (result) {
-  //           // This is the signed-in user
-  //           const user = result.user;
-  //           // This gives you a Google Access Token.
-  //           const credential = GoogleAuthProvider.credentialFromResult(result);
-  //           const token = credential.accessToken;
-  //           setUser(user)
-  //           console.log("USER: " + JSON.stringify(user) + "\nTOKEN: " + token)
-  //       }
-  //    } catch (error) {
-  //     console.error(error)
-  //    }
-  //   }
-  //   redirectResult()
-  // }, [])
+  const [user, setUser] = useState({});
 
   const Navigation = () => {
     if (Object.keys(user).length === 0 && user.constructor === Object) {
-      console.log("USER: " + JSON.stringify(user))
-      return <Login setUser={{setUser}}/>
+      console.log("USER: " + JSON.stringify(user));
+      return <Login setUser={setUser}/>;
     }
     else {
-      console.log("USER: " + JSON.stringify(user))
-      return <h1>Logged in</h1>
+      console.log("USER: " + JSON.stringify(user));
+      return <h1>Logged in</h1>;
     }
   }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -9,23 +9,10 @@ export const Login = ({setUser}) => {
 
     async function login_process() {
         try {
-            await signInWithRedirect(auth, provider);
-            const result = await getRedirectResult(auth);
-            // const user = await signInWithPopup(auth, provider)
-            const credential = GoogleAuthProvider.credentialFromResult(result);
-            const token = credential.accessToken;
-            // The signed-in user info.
-            const user = result.user;
-            console.log("NEW USER: " + JSON.stringify(user))
+            const user = await signInWithPopup(auth, provider);
+            setUser(auth.currentUser)
         } catch (error) {
-            //We can handle errors here, not sure what we want to implement.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            // The email of the user's account used.
-            const email = error.customData.email;
-            // The AuthCredential type that was used.
-            const credential = GoogleAuthProvider.credentialFromError(error);
-            console.log('ERROR: ' + errorMessage)
+            console.log(error);
         }
     }
     return <GoogleButton type="dark" onClick={login_process} />
