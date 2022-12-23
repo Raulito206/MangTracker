7453a2efd6d65a4b45c98c7e893b8ff8731f2193
Alan <alangialya@gmail.com>
2022-12-23 11:48:02 -0800

    Made the UI look cool

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -7,14 +7,24 @@ import "../App.css";
  * @returns JSX.Element
  */
 export const Account:FC = ():JSX.Element => {
+    var userinfo = [
+    'Dummy Username', 
+    'Dummy ID', 
+    'Dummy Display']
+
     return (
         <div>
         <Navbar SelectedTab={2}/>
         <div className={"accounttext"}>
             <ul className={'accounttextul'}>
-                <li>User ID:</li>
-                <li>Email:</li>
-                <li>Display name:</li>
+                <li>User ID: </li>
+                <li>Email: </li>
+                <li>Display: </li>
+            </ul>
+            <ul className={'accounttextul'}>
+                <li>{userinfo[0]}</li>
+                <li>{userinfo[1]}</li>
+                <li>{userinfo[2]}</li>
             </ul>
         </div>
             
